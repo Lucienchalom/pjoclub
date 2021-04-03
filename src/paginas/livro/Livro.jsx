4e18d4f984553000ee736163ae46a282c63dbbb1
lucien <ariel.oliva13@gmail.com>
2021-04-03 19:20:34 -0300

    fontes

--- src/paginas/livro/Livro.jsx
+++ src/paginas/livro/Livro.jsx
@@ -1,12 +1,13 @@
+import "./Livro.css"
 export const Livro = (props) => {
     return (
         <div>
             <h4>{props.nome}</h4>
-            <img src={props.imagem} />
-            <p><strong>Ano:</strong>{props.ano}</p>
-            <p><strong>Série:</strong>{props.serie}</p>
-            <p><strong>Descrição:</strong>{props.descricao}</p>
-            <p><strong>Número de páginas:</strong>{props.paginas}</p>
+            <img alt={props.nome} src={props.imagem} />
+            <p><strong className="lable">Ano: </strong>{props.ano}</p>
+            <p><strong className="lable">Série: </strong>{props.serie}</p>
+            <p><strong className="lable">Descrição: </strong>{props.descricao}</p>
+            <p><strong className="lable">Número de páginas: </strong>{props.paginas}</p>
         </div>
     )
 }
