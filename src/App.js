1451966559113d8ee15f4a18bacb32794c53972a
lucien <ariel.oliva13@gmail.com>
2021-04-13 19:45:58 -0300

    rotas e visual da Livro

--- src/App.js
+++ src/App.js
@@ -1,19 +1,70 @@
 import './App.css';
-import {Livro} from './paginas/livro/Livro'
+import { Livro } from './paginas/livro/Livro'
 
-const livro = {
-  nome: "Ladrão de raios",
-  ano: 2005,
-  serie: "Os olimpianos",
-  paginas: 350,
-  descricao: "The Lightning Thief é um livro juvenil de fantasia e aventura baseado na mitologia grega, escrito por Rick Riordan. É o primeiro livro da série norte-americana Percy Jackson & the Olympians, que narra a vida do adolescente Percy Jackson que descobre ser um semideus, filho de Poseidon com uma humana.",
-  imagem: "https://i.imgur.com/UyytMwD.jpg"
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+const livros = [
+  {
+    id: "ladrao-raios",
+    nome: "Ladrão de raios",
+    ano: 2005,
+    serie: "Os olimpianos",
+    paginas: 350,
+    descricao: "The Lightning Thief é um livro juvenil de fantasia e aventura baseado na mitologia grega, escrito por Rick Riordan. É o primeiro livro da série norte-americana Percy Jackson & the Olympians, que narra a vida do adolescente Percy Jackson que descobre ser um semideus, filho de Poseidon com uma humana.",
+    imagem: "https://i.imgur.com/UyytMwD.jpg"
+  },
+  {
+    id: "mar-monstros",
+    nome: "O Mar de Monstros",
+    ano: 2006,
+    serie: "os olimpianos",
+    paginas: 279,
+    descricao: " Segundo da serie os olimpianos, o trio continua sua aventura atravesando o mar de monstros",
+    imagem: "https://i.imgur.com/w2lIwmf.jpg"
+  }
+]
+
+const LivroPage = () => {
+  let { nome } = useParams();
+  const livrorend = livros.find(livro=> livro.id=== nome)
+  return <Livro {...livrorend} />
 }
 
-function App() {
+export default function App() {
   return (
-    <Livro {...livro} />
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/livro/:nome">
+            <LivroPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (<div>
+    <h2>Livros</h2>
+    <ul>
+      <li>
+        <Link to="/livro/ladrao-raios">Ladrão de Raios</Link>
+      </li>
+      <li>
+        <Link to="/livro/mar-monstros">O Mar de Monstros</Link>
+      </li>
+    </ul>
+  </div>
+  );
+
+}
--- src/paginas/livro/Livro.jsx
+++ src/paginas/livro/Livro.jsx
@@ -1,13 +1,17 @@
 import "./Livro.css"
+import { Link } from "react-router-dom";
 export const Livro = (props) => {
     return (
-        <div>
+        <div id="livro">
+            <Link to = "/">Voltar</Link>
             <h4>{props.nome}</h4>
             <img alt={props.nome} src={props.imagem} />
-            <p><strong className="lable">Ano: </strong>{props.ano}</p>
-            <p><strong className="lable">Série: </strong>{props.serie}</p>
-            <p><strong className="lable">Descrição: </strong>{props.descricao}</p>
-            <p><strong className="lable">Número de páginas: </strong>{props.paginas}</p>
+            <div id="info">
+                <p><strong className="lable">Ano: </strong>{props.ano}</p>
+                <p><strong className="lable">Série: </strong>{props.serie}</p>
+                <p><strong className="lable">Descrição: </strong>{props.descricao}</p>
+                <p><strong className="lable">Número de páginas: </strong>{props.paginas}</p>
+            </div>
         </div>
     )
 }
